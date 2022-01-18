46f4032a3e4affec35d2dce00429bc879922b509
Vinícius Moreira Centorbi <73324369+viniCentorbi@users.noreply.github.com>
2022-01-18 14:52:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ def gerar_refencias(file, dia, mes, ano):
 	input_file.close()
 	output_file.close()
 
-
+#Inserir o arquivo txt com  os links e a data de início aproximada do primeiro site acessado
 gerar_refencias('links.txt', 14, 9, 2021)
